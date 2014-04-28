2a02b2d39a5457c2874268812cc095b7653b93b0
snoooze03 <snoooze03@gmail.com>
2014-04-28 11:53:11 +0200

    Some Advances in SensorSimluations

--- src/main/java/ch/unibnf/mcs/sparklisa/spark/SimpleSparkApp.java
+++ src/main/java/ch/unibnf/mcs/sparklisa/spark/SimpleSparkApp.java
@@ -1,27 +1,18 @@
 package ch.unibnf.mcs.sparklisa.spark;
 
-import java.io.InputStream;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import scala.Tuple2;
 import akka.actor.Props;
 import akka.japi.Creator;
 import ch.unibnf.mcs.sparklisa.receiver.SensorSimulatorActorReceiver;
-import ch.unibnf.mcs.sparklisa.sensor_topology.BasestationType;
 import ch.unibnf.mcs.sparklisa.sensor_topology.NodeType;
-import ch.unibnf.mcs.sparklisa.sensor_topology.Topology;
-import ch.unibnf.mcs.sparklisa.xml.XmlParser;
 
 public class SimpleSparkApp {
 
@@ -36,49 +27,46 @@ public class SimpleSparkApp {
 		return conf;
 	}
 
+	@SuppressWarnings("serial")
 	public static void main(String[] args) throws JAXBException {
-		Topology topology = readXml();
 
 		SparkConf conf = createConf();
 		JavaStreamingContext jssc = new JavaStreamingContext(conf, new Duration(1000L));
 
-		for (BasestationType station : topology.getBasestations().getBasestation()) {
-			handleBaseStation(station, jssc);
-		}
+		Props p = Props.create(new SimpleCreator());
+		JavaDStream<Tuple2<NodeType, Double>> values = jssc.actorStream(p, "Sensor_Receiver");
+		JavaDStream<Tuple2<NodeType, Double>> values2 = jssc.actorStream(p, "Sensor_Receiver2");
+
+		JavaDStream<Tuple2<NodeType, Double>> sum = values
+				.reduce(new Function2<Tuple2<NodeType, Double>, Tuple2<NodeType, Double>, Tuple2<NodeType, Double>>() {
+
+					@Override
+					public Tuple2<NodeType, Double> call(Tuple2<NodeType, Double> t1, Tuple2<NodeType, Double> t2) {
+						NodeType t = new NodeType();
+						Double sum = t1._2() + t2._2();
+						return new Tuple2<NodeType, Double>(t, sum);
+					}
+
+				});
+
+		// JavaPairDStream
+
+		// JavaPairDStream<Double, String> valuesMapped = values.map(new
+		// PairFunction<Tuple2<NodeType, Double>, Double, String>() {
+		//
+		// @Override
+		// public Tuple2<Double, String> call(Tuple2<NodeType, Double> value)
+		// throws Exception {
+		//
+		// return new Tuple2<Double, String>(value._2(),
+		// value._1().getNodeId());
+		// }
+		// });
+
+		sum.print();
 
 		jssc.start();
 		jssc.awaitTermination();
-	}
-
-	private static void handleBaseStation(BasestationType station, JavaStreamingContext jssc) {
-
-		for (JAXBElement<Object> nodeObj : station.getManagedNodes().getNode()) {
-			handleNode(nodeObj, jssc);
-		}
-	}
-
-	@SuppressWarnings("serial")
-	private static void handleNode(JAXBElement<Object> nodeObj, JavaStreamingContext jssc) {
-		NodeType node = (NodeType) nodeObj.getValue();
-		Props p = Props.create(new SimpleCreator());
-		JavaDStream<Double> values = jssc.actorStream(p, "Node_" + node.getNodeId() + "_Receiver");
-
-		JavaPairDStream<Double, String> valuesMapped = values.map(new PairFunction<Double, Double, String>() {
-			@Override
-			public Tuple2<Double, String> call(Double d) throws Exception {
-				return new Tuple2<Double, String>(d, "a");
-			}
-		});
-
-		valuesMapped.print();
-	}
-
-	private static Topology readXml() throws JAXBException {
-		InputStream is = XmlParser.class.getClassLoader().getResourceAsStream("xml/simple_topology.xml");
-		JAXBContext context = JAXBContext.newInstance(Topology.class);
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		Topology t = (Topology) unmarshaller.unmarshal(is);
-		return t;
 	}
 
 	@SuppressWarnings("serial")
--- src/main/scala/ch/unibnf/mcs/sparklisa/receiver/SensorSimulatorActorReceiver.scala
+++ src/main/scala/ch/unibnf/mcs/sparklisa/receiver/SensorSimulatorActorReceiver.scala
@@ -6,17 +6,39 @@ import org.apache.spark.streaming.receivers.Receiver
 import akka.actor.IOManager
 import akka.actor.IO
 import scala.util.Random
+import java.io.InputStream
+import ch.unibnf.mcs.sparklisa.xml.XmlParser
+import scala.io.Source._
+import javax.xml.bind.JAXBContext
+import ch.unibnf.mcs.sparklisa.sensor_topology.Topology
+import ch.unibnf.mcs.sparklisa.sensor_topology.NodeType
+import ch.unibnf.mcs.sparklisa.sensor_topology.BasestationType
+import scala.collection.JavaConversions._
 
 class SensorSimulatorActorReceiver extends Actor with Receiver {
 
-  private final val random = new Random();
+  private final var topology: Topology = readXml()
+  private final val random = new Random()
 
   override def preStart = init()
 
   case class SensonSimulator
 
   def receive = {
-    case _: SensonSimulator => pushBlock(createValue())
+    case _: SensonSimulator => pushNodeBlocks()
+  }
+
+  def pushNodeBlocks() = {
+    Thread.sleep(500L);
+
+    for (station <- topology.getBasestations().getBasestation()) {
+      for (xmlNode <- station.getManagedNodes().getNode()) {
+        var node: NodeType = xmlNode.getValue().asInstanceOf[NodeType]
+        pushBlock((node, random.nextGaussian()))
+      }
+    }
+
+    self ! SensonSimulator()
   }
 
   private def init() = {
@@ -24,10 +46,12 @@ class SensorSimulatorActorReceiver extends Actor with Receiver {
     self ! SensonSimulator()
   }
 
-  private def createValue(): Double = {
-    Thread.sleep(50L);
-    self ! SensonSimulator()
-    return random.nextGaussian();
+  private def readXml(): Topology = {
+    var is = getClass().getClassLoader().getResourceAsStream("xml/simple_topology.xml")
+    var context = JAXBContext.newInstance(classOf[Topology])
+    var unmarshaller = context.createUnmarshaller()
+    var t: Topology = unmarshaller.unmarshal(is).asInstanceOf[Topology]
+    return t;
   }
 
 }
