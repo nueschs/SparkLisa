f719be43d1c4423314b3b9a7d3596679850d3c54
snoooze03 <snoooze03@gmail.com>
2014-08-19 13:26:40 +0200

    value generator per base station

--- src/main/python/ValueFileGenerator.py
+++ src/main/python/ValueFileGenerator.py
@@ -46,29 +46,42 @@ def one_line_node_id_value(num_nodes_, num_values_):
         count += 1
     file_name = file_name.replace('%', str(count))
     file_ = open(file_name, 'wb')
-    for i in range(1, int(num_nodes_)+1):
+    for i in range(1, int(num_nodes_) + 1):
         for j in range(0, int(num_values_)):
-            line = 'node'+str(i)+';'+str(gauss(0.0, 1.0))+'\n'
+            line = 'node' + str(i) + ';' + str(gauss(0.0, 1.0)) + '\n'
             file_.write(line)
 
-def one_file_per_basestation(num_nodes_, num_values_, num_basestations_):
-    base_file_name = '../resources/node_values/per_base_'+str(num_basestations_)+'/'
+
+def one_file_per_basestation(num_nodes_, num_values_, num_base_stations_):
+    base_file_name = '../resources/node_values/per_base_' + str(num_base_stations_) + '/'
     if not os.path.isdir(base_file_name):
         os.makedirs(base_file_name)
 
+    if int(num_nodes_) % num_base_stations_ > 0:
+        print("Number of nodes has to be a multiple of number of base stations")
+        sys.exit(0)
+
+    nodes_per_base = int(num_nodes_) / int(num_base_stations_)
+    for i in range(0, num_base_stations_):
+        file_ = open(base_file_name + str(i) + '_' + num_nodes_ + '_' + num_values_ + '.txt', 'wb')
+        for j in range(i * nodes_per_base, (i + 1) * nodes_per_base):
+            node_id = 'node' + str(j)
+            for k in range(0, int(num_values_)):
+                file_.write(node_id + ';' + str(gauss(0.0, 1.0)) + '\n')
 
 
-
-def run_mode(mode_, num_nodes_, num_values_):
+def run_mode(mode_, num_nodes_, num_values_, num_base_stations_):
     return {
         1: create_single_file(num_nodes_, num_values_),
         2: create_file_per_node(num_nodes_, num_values_),
-        3: one_line_node_id_value(num_nodes_, num_values_)
+        3: one_line_node_id_value(num_nodes_, num_values_),
+        4: one_file_per_basestation(num_nodes_, num_values_, num_base_stations_)
     }.get(mode_, None)
 
 
 mode, num_nodes, num_values = sys.argv[1], sys.argv[2], sys.argv[3]
-run_mode(mode, num_nodes, num_values)
+num_base_stations = int(sys.argv[4]) if mode == '4' else None
+run_mode(mode, num_nodes, num_values, num_base_stations)
 
 
 
