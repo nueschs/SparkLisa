bbd44ddd73b345e70f153434a05b83f697fabc9b
snoooze03 <snoooze03@gmail.com>
2014-08-23 10:37:03 +0200

    try create and upload values only

--- src/main/python/cluster_runner.py
+++ src/main/python/cluster_runner.py
@@ -99,7 +99,7 @@ def upload_values(num_files, num_values, num_nodes, num_base_stations, window_):
             hdfs_client.mkdir(['/sparkLisa/values/{0}_{1}/{2}'.format(num_nodes, num_base_stations, j+1)], create_parent=True).next()
             file_name = '{0}_{1}_{2}.txt'.format(num_nodes, num_values, j)
             src_file = base_path + '{0}/{1}'.format(j+1, file_name)
-            dst_file = hdfs_base_path +'${0}/'.format(j+1)
+            dst_file = hdfs_base_path +'{0}/'.format(j+1)
             command = ['hadoop', 'fs', '-copyFromLocal', src_file, dst_file]
             call(command)
         time.sleep(window_)
