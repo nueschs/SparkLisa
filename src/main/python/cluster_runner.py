069a7eed51341f140cca6570cbe8263475ce0f71
snoooze03 <snoooze03@gmail.com>
2014-09-03 17:21:47 +0200

    minor fix

--- src/main/python/cluster_runner.py
+++ src/main/python/cluster_runner.py
@@ -98,7 +98,7 @@ def upload_values(num_files, num_values, num_nodes, num_base_stations, window_, 
     hdfs_base_path = hdfs_path+'values/{0}_{1}/'.format(num_nodes, num_base_stations)
     for i in range(0, num_files):
         for j in range(0, num_base_stations):
-            temp_path = base_path+'temp/{0}/'.format(j)
+            temp_path = base_path+'temp/{0}/'.format(j+1)
             os.mkdir(temp_path)
             hdfs_client.mkdir(['/user/stefan/sparkLisa/values/{0}_{1}/{2}'.format(num_nodes, num_base_stations, j+1)], create_parent=True).next()
             file_name = '{0}_{1}_{2}.txt'.format(num_nodes, num_values, i)
