9a2ff1f9d751be7229af4d2cbc55a18d3ece9429
snoooze03 <snoooze03@gmail.com>
2014-09-08 17:01:25 +0200

    new approach for receiver

--- src/main/scala/ch/unibnf/mcs/sparklisa/app/TestApp.scala
+++ src/main/scala/ch/unibnf/mcs/sparklisa/app/TestApp.scala
@@ -5,6 +5,7 @@ import java.util.Properties
 
 import akka.actor.{Props, ActorSystem, Actor, ActorRef}
 import akka.io.{IO, Tcp}
+import ch.unibnf.mcs.sparklisa.app.FileInputLisaStreamingJob._
 import ch.unibnf.mcs.sparklisa.receiver.TestReceiver
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.DStream
@@ -20,18 +21,14 @@ object TestApp {
 //  val Master: String = "local[2]"
   val Master: String = "spark://saight02:7077"
   var gt: Thread = null
-  var Env: String = null
 
-  def createSparkConf(): SparkConf = {
-    val conf: SparkConf = new SparkConf()
-    conf.setAppName("Simple Streaming App")
-      .setMaster(Master)
-      .setSparkHome("/home/snoooze/spark/spark-1.0.0")
-      .setJars(Array[String]("target/SparkLisa-0.0.1-SNAPSHOT.jar"))
-    return conf
-  }
+  val config: Properties = new Properties()
+  var Env: String = null
+  var HdfsPath: String = null
+  var Strategy = None: Option[String]
 
   def main(args: Array[String]){
+    initConfig()
     val conf: SparkConf = createSparkConf()
     val ssc: StreamingContext = new StreamingContext(conf, Seconds(1))
 
@@ -72,6 +69,25 @@ object TestApp {
 
     ssc.start()
     ssc.awaitTermination()
+  }
+
+  private def createSparkConf(): SparkConf = {
+    val conf: SparkConf = new SparkConf()
+    conf.setAppName("File Input LISA Streaming Job")
+    if ("local" == Env) {
+      conf.setMaster(Master)
+        .setSparkHome("/home/snoooze/spark/spark-1.0.0")
+        .setJars(Array[String]("target/SparkLisa-0.0.1-SNAPSHOT.jar"))
+    }
+
+    return conf
+  }
+
+  private def initConfig() = {
+    config.load(getClass.getClassLoader.getResourceAsStream("config.properties"))
+    Env = config.getProperty("build.env")
+    HdfsPath = config.getProperty("hdfs.path." + Env)
+    Strategy = Some(config.getProperty("receiver.strategy"))
   }
 
 //  class Generator(pos: Int, masterHost: String) extends Actor {
