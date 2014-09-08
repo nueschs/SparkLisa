39244647c1a473609e04086244484389d54afbad
snoooze03 <snoooze03@gmail.com>
2014-09-08 14:11:28 +0200

    new approach for receiver

--- src/main/scala/ch/unibnf/mcs/sparklisa/app/TestApp.scala
+++ src/main/scala/ch/unibnf/mcs/sparklisa/app/TestApp.scala
@@ -20,9 +20,10 @@ object TestApp {
 
   def createSparkConf(): SparkConf = {
     val conf: SparkConf = new SparkConf()
-    conf.setAppName("Simple Streaming App").setMaster(Master)
-      .setSparkHome("/home/snoooze/spark/spark-1.0.0")
-      .setJars(Array[String]("target/SparkLisa-0.0.1-SNAPSHOT.jar"))
+    conf.setAppName("Simple Streaming App")
+//      .setMaster(Master)
+//      .setSparkHome("/home/snoooze/spark/spark-1.0.0")
+//      .setJars(Array[String]("target/SparkLisa-0.0.1-SNAPSHOT.jar"))
     return conf
   }
 
