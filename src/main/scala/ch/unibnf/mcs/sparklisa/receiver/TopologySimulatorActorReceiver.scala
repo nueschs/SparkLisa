32305d4b094016a5d9fa631b82596d11194c319c
snoooze03 <snoooze03@gmail.com>
2014-09-15 11:10:16 +0200

    minor fix

--- src/main/scala/ch/unibnf/mcs/sparklisa/receiver/TopologySimulatorActorReceiver.scala
+++ src/main/scala/ch/unibnf/mcs/sparklisa/receiver/TopologySimulatorActorReceiver.scala
@@ -13,7 +13,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class TopologySimulatorActorReceiver(nodes: List[NodeType], rate: Int) extends Actor with ActorHelper {
 
   val random = new Random()
-  private val sleepDuration: Int = ((rate / 60.0) * 1000).toInt
+  private val sleepDuration: Int = ((rate * 1000)/ 60.0).toInt
 
   override def preStart = {
     context.system.scheduler.schedule((sleepDuration * 3.5) milliseconds, sleepDuration milliseconds)({
