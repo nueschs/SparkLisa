50096f2963dec4cba7eab3302e7f0d70f5f23579
snoooze03 <snoooze03@gmail.com>
2014-09-15 15:29:20 +0200

    minor fix

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-lines=$(hadoop fs -ls -d $1/values* | awk '{print $8}')
+lines=$(hadoop fs -ls -d $1/results/1_1600/allValues* | awk '{print $8}')
 
 for line in $lines
 do
-	echo `hadoop fs -text $line/part* | wc -l` 
+	echo `hadoop fs -text $line/part* | wc -l`
 done
+
 
