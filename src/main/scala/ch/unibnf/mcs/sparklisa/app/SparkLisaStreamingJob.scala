e43cf4347a413b4663e45e6bfd73107af09a54db
snoooze03 <snoooze03@gmail.com>
2014-09-15 12:20:04 +0200

    minor fix

--- src/main/scala/ch/unibnf/mcs/sparklisa/app/SparkLisaStreamingJob.scala
+++ src/main/scala/ch/unibnf/mcs/sparklisa/app/SparkLisaStreamingJob.scala
@@ -82,9 +82,7 @@ object SparkLisaStreamingJob {
     val numberOfBaseStations = topology.getBasestation.size().toString
     val numberOfNodes = topology.getNode.size().toString
     allValues.saveAsTextFiles(HdfsPath + s"/results/${numberOfBaseStations}_$numberOfNodes/allValues")
-    runningCount.saveAsTextFiles(HdfsPath + s"/results/${numberOfBaseStations}_$numberOfNodes/allCount")
     finalLisaValues.saveAsTextFiles(HdfsPath + s"/results/${numberOfBaseStations}_$numberOfNodes/finalLisaValues")
-    finalLisaValues.count().saveAsTextFiles(HdfsPath + s"/results/${numberOfBaseStations}_$numberOfNodes/finalCount")
 
     ssc.start()
     ssc.awaitTermination(timeout*1000)
