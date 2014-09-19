7641f6d56151b8378a9be0a1c46d05515062443c
snoooze03 <snoooze03@gmail.com>
2014-09-19 16:14:54 +0200

    added stats results collection

---
+++ collect_stats_values.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd temp
+hadoop fs -text sparkLisa/results/$1_$2/measuredValuesPositions-$3/part* > mvp
+hadoop fs -text sparkLisa/results/$1_$2/finalLisaValues-$3/part* > flv
+hadoop fs -text sparkLisa/results/$1_$2/lisaValuesWithRandomNeighbourIds-$3/part* > lvwni
+hadoop fs -text sparkLisa/results/$1_$2/allValues-$3/part* > av
+hadoop fs -text sparkLisa/results/$1_$2/t0-$3/part* > lvwrnlv
+cd ..
+tar cfz results_$1_$2_$3.tar.gz temp/*
