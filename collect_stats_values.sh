8616fb3e3ca27d0dac5d2169a7f1da7e50b78042
snoooze03 <snoooze03@gmail.com>
2014-09-20 20:16:44 +0200

    minor fix

--- collect_stats_values.sh
+++ collect_stats_values.sh
@@ -5,6 +5,6 @@ hadoop fs -text sparkLisa/results/$1_$2/finalLisaValues-$3/part* > flv
 hadoop fs -text sparkLisa/results/$1_$2/allValues-$3/part* > av
 hadoop fs -text sparkLisa/results/$1_$2/allLisaValues-$3/part* > alv
 hadoop fs -text sparkLisa/results/$1_$2/measuredValuesPositions-$3/part* > mvp
-hadoop fs -copyToLocal /sparkLisa/topology/topology_bare_connected_$2.txt
+hadoop fs -copyToLocal sparkLisa/topology/topology_bare_connected_$2.txt
 cd ..
-#tar cfz results_$1_$2_$3.tar.gz temp/*
+tar cfz results_$1_$2_$3.tar.gz temp/*
